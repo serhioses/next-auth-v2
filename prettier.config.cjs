84d59e0b53dbd035dc2fab5adb71281cf29c095f
sergii-myroshnyk-windmill <sergii.myroshnyk@windmill.digital>
2025-03-31 16:20:36 +0300

    Init the project

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+module.exports = {
+  plugins: ['prettier-plugin-tailwindcss'],
+  tabWidth: 4,
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+};
